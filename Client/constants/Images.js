41cf4257779e8a31130abdad898746e8aad392e2
Bryan Young <bryan.young@usm.edu>
2020-05-04 21:52:42 -0500

    Fixes I think

---
+++ Client/constants/Images.js
@@ -0,0 +1,49 @@
+// local imgs
+const Onboarding = require("../assets/imgs/bg.jpg");
+const Logo = require("../assets/imgs/SH_Logo.png");
+const LogoOnboarding = require("../assets/imgs/SH_Logo_white.png");
+const ProfileBackground = require("../assets/imgs/signin_bg.png");
+const RegisterBackground = require("../assets/imgs/signin_bg.png");
+const ArgonLogo = require("../assets/imgs/SH_Logo.png");
+const iOSLogo = require("../assets/imgs/ios.png");
+const androidLogo = require("../assets/imgs/android.png");
+const receipt1 = require("../assets/imgs/Receipt1.jpg");
+const receipt2 = require("../assets/imgs/receipt2.jpeg");
+const receipt3 = require("../assets/imgs/receipt3.jpg");
+const receipt4 = require("../assets/imgs/receipt4.jpg");
+const receipt5 = require("../assets/imgs/receipt5.jpg");
+
+// internet imgs
+
+const ProfilePicture = 'https://images.unsplash.com/photo-1492633423870-43d1cd2775eb?fit=crop&w=1650&q=80';
+
+const Viewed = [
+  '../assets/imgs/Receipt1.jpg',
+  '../assets/imgs/Receipt2.jpeg',
+  '../assets/imgs/Receipt3.jpg',
+  '../assets/imgs/Receipt4.jpg',
+  '../assets/imgs/Receipt5.jpg',
+];
+
+const Statistics = {
+    'View article': 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fapexcharts.com%2Freact-chart-demos%2Fpie-charts%2F&psig=AOvVaw1JpX6LOPD5OOgLQazM-S6J&ust=1586869832972000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCPjxn_q85egCFQAAAAAdAAAAABAD',
+};
+
+export default {
+  Onboarding,
+  Logo,
+  LogoOnboarding,
+  ProfileBackground,
+  ProfilePicture,
+  RegisterBackground,
+  Viewed,
+  ArgonLogo,
+  iOSLogo,
+    androidLogo,
+    receipt1,
+    receipt2,
+    receipt3,
+    receipt4,
+    receipt5,
+    Statistics
+};
---
+++ Client/constants/receipts.js
@@ -0,0 +1,35 @@
+
+
+
+export default [
+    {
+        title: 'Receipt ###### ##/##/####',
+        image: './assets/imgs/Receipt1.jpg',
+        cta: 'View Receipt',
+        horizontal: true
+    },
+    {
+        title: 'Receipt ###### ##/##/####',
+        image: './assets/imgs/receipt2.jpeg',
+        cta: 'View Receipt',
+        horizontal: true
+    },
+    {
+        title: 'Receipt ###### ##/##/####',
+        image: './assets/imgs/receipt3.jpg',
+        cta: 'View Receipt',
+        horizontal: true
+    },
+    {
+        title: 'Receipt ###### ##/##/####',
+        image: './assets/imgs/receipt4.jpg',
+        cta: 'View Receipt',
+        horizontal:true
+    },
+    {
+        title: 'Receipt ###### ##/##/####',
+        image: './assets/imgs/receipt5.jpg',
+        cta: 'View Receipt',
+        horizontal: true
+    },
+];
